f496e86cd3398e81039d6fcd3bc97168c5b9bc49
sam <sam.wilson@dizplai.com>
2025-12-21 17:04:51 +0000

    test commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h2 class="text-xl font-semibold text-gray-800 mb-4">Welcome to Trackd</h2>
-    <p class="text-gray-600">Your Vue + Tailwind app is ready.</p>
+    <p class="text-gray-600">Your Vue app is ready</p>
   </div>
 </template>
